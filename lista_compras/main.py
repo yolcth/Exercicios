5239125b572fc31c944cd2be6db2a3b1896f5196
Matheus Lacerda <matheusaraujodlc@gmail.com>
2026-07-22 17:52:32 -0300

    Tratamento de Erro: Adicionei tratamentos de erros no sistema de compras

--- lista_compras/main.py
+++ lista_compras/main.py
@@ -1,34 +1,41 @@
 import uteis; from interface import inter; from time import sleep
 
-arquivo = 'python/projects.py/lista_compras/historico.json'
+arquivo = 'python/projects/lista_compras/historico.json'
 inter.cabecalho('Sistema de Lista de Compras.')
 print()
 
-try:
-    while True:
-        lista = [
-            '1 - Histórico de Compras;',
-            '2 - Comprar e Registrar um novo produto;',
-            '3 - Sair do Programa'
-        ]
-        for item in lista:
-            print(item)
+lista = [
+    '1 - Histórico de Compras;',
+    '2 - Comprar e Registrar um novo produto;',
+    '3 - Sair do Programa'
+]
+
+while True:
+    for item in lista:
+        print(item)
+    try:
         ler = int(input('\nQual opção você deseja realizar? [1/3]: '))
-        if ler == 1:
-            print()
-            uteis.ler(arquivo)
-            print('-' * 35); print()
-        elif ler == 2:
-            inter.cabecalho('Cadastrando Produto')
+    except ValueError:
+        print('\033[0;31mNúmero inválido. Tente novamente.\033[m')
+        continue
+    if ler == 1:
+        print()
+        uteis.ler(arquivo)
+        print('-' * 35); print()
+    elif ler == 2:
+        inter.cabecalho('Cadastrando Produto')
+        try:
             produto = str(input('Nome do produto: '))
             quantidade = int(input('Quantidade: '))
-            uteis.cadastrar(arquivo, produto, quantidade); sleep(0.5); print('Cadastrando..'); sleep(0.5); print('..'); sleep(0.5)
-            print(f'Produto cadastrado!')
-            print('-' * 35); print()
-        elif ler == 3:
-            print('Muito obrigado! Volte sempre!')
-            break
-        else:
+        except NameError:
             print('\033[0;31mNúmero inválido. Tente novamente.\033[m')
-except:
-    print('\033[0;31mErro. Tente novamente.\033[m')
+            continue
+        uteis.cadastrar(arquivo, produto, quantidade)
+        sleep(0.5); print('Cadastrando..'); sleep(0.5); print('..'); sleep(0.5)
+        print(f'Produto cadastrado!')
+        print('-' * 35); print()
+    elif ler == 3:
+        print('Muito obrigado! Volte sempre!')
+        break
+    else:
+        print('\033[0;31mNúmero inválido. Tente novamente.\033[m')
--- lista_compras/uteis.py
+++ lista_compras/uteis.py
@@ -1,15 +1,16 @@
 import json; from interface import inter
 
 def cadastrar(arquivo, nome='Desconhecido', quantidade=0):
-
+    try:
         with open(arquivo, 'r') as a:
             dados = json.load(a)
-
         novo_item = {'nome': nome, 'quantidade': quantidade}
         dados.append(novo_item)
-        
         with open(arquivo, 'w') as a:
             json.dump(dados, a, indent=4)
+    except (json.JSONDecodeError, FileNotFoundError):
+        print('\033[31mErro ao abrir o arquivo.\033[m')
+            
 
 def ler(arquivo):
     try:
@@ -18,5 +19,6 @@ def ler(arquivo):
             dados = json.load(a)
             for item in dados:
                 print(f"Produto: {item['nome']} - Quantidade: {item['quantidade']}")
-    except:
-        print('\033[0;31mErro. Tente novamente.\033[m')
+    except (json.JSONDecodeError, FileNotFoundError):
+         print('\033[31mErro ao abrir o arquivo.\033[m')
+        
